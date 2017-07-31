002bc18e422abe5d857866fd9a27e5a96768c047
zhangzhao <zz275321945@163.com>
2017-07-31 15:48:33 +0800

    打包2.0

--- app/src/main/java/com/zhang/znotes/MyApplication.java
+++ app/src/main/java/com/zhang/znotes/MyApplication.java
@@ -5,20 +5,17 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Environment;
 
+import com.taobao.sophix.PatchStatus;
 import com.taobao.sophix.SophixManager;
 import com.taobao.sophix.listener.PatchLoadStatusListener;
-import com.taobao.sophix.util.PatchStatus;
 import com.zhang.znotes.log.CrashExceptionHandler;
-import com.zhang.znotes.utils.LogUtil;
 
 import org.litepal.LitePal;
 
 import java.io.File;
-import java.util.concurrent.TimeUnit;
 
 import cn.bmob.v3.Bmob;
 import cn.bmob.v3.BmobConfig;
-import okhttp3.OkHttpClient;
 import skin.support.SkinCompatManager;
 import skin.support.design.app.SkinMaterialViewInflater;
 
@@ -74,22 +71,17 @@ public class MyApplication extends Application {
      * 设置热修复初始化
      */
     private void setHotFix() {
-        try {
-            this.appVersion = this.getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName;
-        } catch (Exception e) {
-            this.appVersion = "1.0";
-        }
+        // initialize最好放在attachBaseContext最前面
         SophixManager.getInstance().setContext(this)
                 .setAppVersion(appVersion)
                 .setAesKey(null)
                 .setEnableDebug(true)
                 .setPatchLoadStatusStub(new PatchLoadStatusListener() {
                     @Override
-                    public void onload(final int mode, final int code, final String info, final int handlePatchVersion) {
+                    public void onLoad(final int mode, final int code, final String info, final int handlePatchVersion) {
                         // 补丁加载回调通知
                         if (code == PatchStatus.CODE_LOAD_SUCCESS) {
                             // 表明补丁加载成功
-                            LogUtil.e("热补丁加载成功");
                         } else if (code == PatchStatus.CODE_LOAD_RELAUNCH) {
                             // 表明新补丁生效需要重启. 开发者可提示用户或者强制重启;
                             // 建议: 用户可以监听进入后台事件, 然后应用自杀
@@ -98,11 +90,12 @@ public class MyApplication extends Application {
                             // SophixManager.getInstance().cleanPatches();
                         } else {
                             // 其它错误信息, 查看PatchStatus类说明
-                            LogUtil.e("热更新 其他更新错误 code=" + code+",mode="+mode+",info="+info+",handlePatchVersion="+handlePatchVersion);
                         }
                     }
                 }).initialize();
+// queryAndLoadNewPatch不可放在attachBaseContext 中，否则无网络权限，建议放在后面任意时刻，如onCreate中
         SophixManager.getInstance().queryAndLoadNewPatch();
+
     }
 
     /**
